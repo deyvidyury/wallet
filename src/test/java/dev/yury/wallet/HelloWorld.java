8638fcf3ad73e49fa777bd4010adfcfa0f34b431
Deyvid Lopes <deyvidyury@yahoo.com.br>
2023-04-11 18:33:12 -0300

    Configuracao de testes

---
+++ src/test/java/dev/yury/wallet/HelloWorld.java
@@ -0,0 +1,15 @@
+package dev.yury.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class HelloWorld
+{
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1,1);
+    }
+}
